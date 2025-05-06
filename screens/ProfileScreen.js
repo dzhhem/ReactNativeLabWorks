a5f2c9f089b1c108ccabe37271f2b25969a71bb6
Makar Dzhehur <vt231_dmyu@student.ztu.edu.ua>
2025-05-06 11:08:26 +0300

    feat: add ProfileScreen functionality

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,9 +1,32 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableHighlight } from 'react-native';
 
 export default function ProfileScreen() {
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>Реєстрація</Text>
 
+            <Text style={styles.label}>Електронна пошта</Text>
+            <TextInput style={styles.input} keyboardType="email-address" />
+
+            <Text style={styles.label}>Пароль</Text>
+            <TextInput style={styles.input} secureTextEntry={true} />
+
+            <Text style={styles.label}>Пароль (ще раз)</Text>
+            <TextInput style={styles.input} secureTextEntry={true} />
+
+            <Text style={styles.label}>Прізвище</Text>
+            <TextInput style={styles.input} />
+
+            <Text style={styles.label}>Ім’я</Text>
+            <TextInput style={styles.input} />
+
+            <TouchableHighlight
+                style={styles.button}
+                underlayColor="#005ac1"
+                onPress={() => alert("Зареєстровано")}
+            >
+                <Text style={styles.buttonText}>Зареєструватися</Text>
+            </TouchableHighlight>
         </View>
     );
 }
@@ -11,7 +34,38 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 10,
-        backgroundColor: '#fff'
+        paddingTop: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#fff',
     },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 18,
+    },
+    label: {
+        fontSize: 14,
+        marginBottom: 5,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 2,
+        padding: 10,
+        marginBottom: 10,
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        height: 38,
+        backgroundColor: '#0073ff',
+        marginTop: 14,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#fff',
+    }
 });
