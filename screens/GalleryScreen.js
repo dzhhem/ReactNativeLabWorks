68d02434cf9646f4211cb5f72bb81fa820a51096
Makar Dzhehur <vt231_dmyu@student.ztu.edu.ua>
2025-05-06 11:07:08 +0300

    feat: add GalleryScreen functionality

--- screens/GalleryScreen.js
+++ screens/GalleryScreen.js
@@ -1,9 +1,16 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+
+const data = new Array(12).fill(null).map((_, i) => ({ id: i.toString() }));
 
 export default function GalleryScreen() {
     return (
         <View style={styles.container}>
-
+            <FlatList
+                data={data}
+                numColumns={2}
+                keyExtractor={(item) => item.id}
+                renderItem={() => <View style={styles.imageBox} />}
+            />
         </View>
     );
 }
@@ -11,7 +18,15 @@ export default function GalleryScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 10,
-        backgroundColor: '#fff'
+        paddingTop: 10,
+        paddingHorizontal: 10,
+        backgroundColor: '#fff',
+    },
+    imageBox: {
+        width: '45%',
+        aspectRatio: 1.50,
+        backgroundColor: '#eee',
+        margin: '2.5%',
+        borderRadius: 6,
     },
 });
